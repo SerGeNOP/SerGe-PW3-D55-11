d7db62e9d326a1bc38c20a0d16a0ffdf13b439c1
SerGeNOP <3750101@mail.ru>
2020-02-16 17:41:40 +0700

    add allowed hosts

--- D55/settings.py
+++ D55/settings.py
@@ -82,14 +82,14 @@ WSGI_APPLICATION = 'D55.wsgi.application'
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 
-DATABASES = {'default': dj_database_url.config(default=os.environ.get('DATABASE_URL')), 'ENGINE': 'django.db.backends.postgresql'}
+# DATABASES = {'default': dj_database_url.config(default=os.environ.get('DATABASE_URL')), 'ENGINE': 'django.db.backends.postgresql'}
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 
 
 # Password validation
